2f4ef93c19d84240332a7b5e632a0a4884f810d1
abyanji <a.byanjil99@gmail.com>
2023-01-28 15:55:42 +0300

    Change Task01

--- Task01.py
+++ Task01.py
@@ -7,20 +7,29 @@
 # else:
 #     print(b)
 
-a = int(input())
-if 1000 <= a <= 9999 or -9999 <= a <= -1000:
-    print('YES')
-else:
-    print('NO')
+# a = int(input())
+# if 1000 <= a <= 9999 or -9999 <= a <= -1000:
+#     print('YES')
+# else:
+#     print('NO')
 
-a = input()
-if '-' in a:
-    if len(a) == 5:
-        print('YES')
-    else:
-        print('NO')
-else:
-    if len(a) == 4
-        print('YES')
-    else:
-        print('NO')
+# a = input()
+# if '-' in a:
+#     if len(a) == 5:
+#         print('YES')
+#     else:
+#         print('NO')
+# else:
+#     if len(a) == 4
+#         print('YES')
+#     else:
+#         print('NO')
+
+summa = 0
+while True:
+    a = input()
+    if a == ' ':
+        break
+    if int(a) % 4 == 0:
+        summa += int(a)
+print(summa)
